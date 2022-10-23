cb5b08b3b2cb4dccb2767abb01ae4e9c63ed272e
adepier <adepier@gmail.com>
2022-10-23 17:34:09 +0200

    ok

--- test_interruptions.cpp
+++ test_interruptions.cpp
@@ -9,24 +9,33 @@ class Counter {
 public:
     Counter(PinName pin1,PinName pin2) : _interrupt1(pin1) ,_interrupt2(pin2)         // create the InterruptIn on the pin specified to Counter
     {
-        _interrupt1.rise(callback(this, &Counter::increment)); // attach increment function of this counter instance
-        _interrupt2.rise(callback(this, &Counter::increment)); // attach increment function of this counter instance
+        _interrupt1.rise(callback(this, &Counter::increment1)); // attach increment function of this counter instance
+        _interrupt2.rise(callback(this, &Counter::increment2)); // attach increment function of this counter instance
     }
 
-    void increment()
+    void increment1()
     {
-        _count++;
+        _count1++;
     }
 
-    int read()
+    int read1()
     {
-        return _count;
+        return _count1;
+    }
+    void increment2()
+    {
+        _count2++;
     }
 
+    int read2()
+    {
+        return _count2;
+    }
 private:
     InterruptIn _interrupt1;
     InterruptIn _interrupt2;
-    volatile int _count;
+    volatile int _count1;
+    volatile int _count2;
 };
 
 
@@ -40,7 +49,12 @@ Counter counter5(D10,D11);
 int main()
 {
     while (1) {
-        printf("Count so far: %d\n", counter1.read());
+        printf("Count1 so far 1: %d  / 2 : %d\n", counter1.read1(), counter1.read2());
+        printf("Count2 so far 1: %d  / 2 : %d\n", counter2.read1(), counter2.read2());
+        printf("Count3 so far 1: %d  / 2 : %d\n", counter3.read1(), counter3.read2());
+        printf("Count4 so far 1: %d  / 2 : %d\n", counter4.read1(), counter4.read2());
+        printf("Count5 so far 1: %d  / 2 : %d\n", counter5.read1(), counter5.read2());
+        
         ThisThread::sleep_for(2000);
     }
 }
