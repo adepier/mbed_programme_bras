5c167d347e4e290b0274505e9b882e5c7d5cc814
adepier <adepier@gmail.com>
2022-10-27 21:29:38 +0200

    ok

--- test_interruptions.cpp
+++ test_interruptions.cpp
@@ -7,9 +7,21 @@
  #include "Pin_interrupt.h" 
 
 Encoder encoder1;
+Encoder encoder2;
+Encoder encoder3;
+Encoder encoder4;
+Encoder encoder5;
 
 Pin_interrupt counter1_1(D2,1,encoder1 );
-Pin_interrupt counter1_2(D3,2,encoder1  );
+Pin_interrupt counter1_2(D3,2,encoder1 );
+Pin_interrupt counter2_1(D4,1,encoder2 );
+Pin_interrupt counter2_2(D5,2,encoder2 );
+Pin_interrupt counter3_1(D6,1,encoder3 );
+Pin_interrupt counter3_2(D9,2,encoder3 );
+Pin_interrupt counter4_1(D10,1,encoder4 );
+Pin_interrupt counter4_2(D11,2,encoder4 );
+Pin_interrupt counter5_1(D12,1,encoder5 );
+Pin_interrupt counter5_2(D13,2,encoder5 );
 // MyInterrupt counter4(D4 );
 // MyInterrupt counter5(D5 );
 // MyInterrupt counter6(D6 );
@@ -36,6 +48,10 @@ int main()
 {
     while (1) {
          printf("Count1 :   %d:   %d:   %d\n", encoder1.count,encoder1.tic_backward,encoder1.tic_forward);
+         printf("Count1 :   %d:   %d:   %d\n", encoder2.count,encoder2.tic_backward,encoder2.tic_forward);
+         printf("Count1 :   %d:   %d:   %d\n", encoder3.count,encoder3.tic_backward,encoder3.tic_forward);
+         printf("Count1 :   %d:   %d:   %d\n", encoder4.count,encoder4.tic_backward,encoder4.tic_forward);
+         printf("Count1 :   %d:   %d:   %d\n", encoder5count,encoder5.tic_backward,encoder5.tic_forward);
 //  printf("Count3 :   %d  \n", counter3.read1());
 //  printf("Count4 :   %d  \n", counter4.read1());
 //  printf("Count5 :   %d  \n", counter5.read1());
@@ -59,7 +75,7 @@ int main()
 
 
         // printf("Count5 so far 1: %d  / 2 : %d\n\n\n", counter5.read1(), counter5.read2());
-      //  printf("\n\n\n");
+         printf("\n\n\n");
         ThisThread::sleep_for(2000);
     }
 };
