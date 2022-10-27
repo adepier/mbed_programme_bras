92df4bb4a6e5aca32182c1133ada78e1b546b7d5
adepier <adepier@gmail.com>
2022-10-27 09:30:21 +0200

    ok

--- test_interruptions.cpp
+++ test_interruptions.cpp
@@ -7,13 +7,11 @@
 
 class Counter {
 public:
-    Counter(PinName pin1,PinName pin2) : _interrupt1(pin1) ,_interrupt2(pin2)         // create the InterruptIn on the pin specified to Counter
+    Counter(PinName pin1 ) : _interrupt1(pin1,PullDown)          // create the InterruptIn on the pin specified to Counter
     {   
-        _interrupt1.enable_irq();
-        _interrupt2.enable_irq();
+        _interrupt1.enable_irq(); 
 
-        _interrupt1.rise(callback(this, &Counter::increment1)); // attach increment function of this counter instance
-        _interrupt2.rise(callback(this, &Counter::increment2)); // attach increment function of this counter instance
+        _interrupt1.rise(callback(this, &Counter::increment1)); // attach increment function of this counter instance 
     }
 
     void increment1()
@@ -25,37 +23,64 @@ public:
     {
         return _count1;
     }
-    void increment2()
-    {
-        _count2++;
-    }
-
-    int read2()
-    {
-        return _count2;
-    }
+    
 private:
     InterruptIn _interrupt1;
-    InterruptIn _interrupt2;
-    volatile int _count1;
-    volatile int _count2;
+     
+    volatile int _count1; 
 };
 
 
-Counter counter1(D2,D3);
-Counter counter2(D4,D5);
-Counter counter3(D6,D12);
- Counter counter4(D9,D11);
- Counter counter5(A4,A3);
+Counter counter2(D2 );
+Counter counter3(D3 );
+Counter counter4(D4 );
+Counter counter5(D5 );
+Counter counter6(D6 );
+Counter counter7(D7 );
+Counter counter8(D8 );
+Counter counter9(D9 );
+Counter counter10(D10 );
+Counter counter11(D11 );
+Counter counter12(D12 );
+Counter counter13(D13 );
+//
+// Counter counterA0(A0);
+// Counter counterA1(A1);
+// Counter counterA2(A2);
+// Counter counterA3(A3);
+// Counter counterA4(A4);
+// Counter counterA5(A5);
+// Counter counterA6(A6);
+// Counter counterA7(A7);
+
 
 
 int main()
 {
     while (1) {
-        printf("Count1 so far 1: %d  / 2 : %d\n", counter1.read1(), counter1.read2());
-        printf("Count2 so far 1: %d  / 2 : %d\n", counter2.read1(), counter2.read2());
-        printf("Count3 so far 1: %d  / 2 : %d\n", counter3.read1(), counter3.read2());
-         printf("Count4 so far 1: %d  / 2 : %d\n", counter4.read1(), counter4.read2());
+         printf("Count2 :   %d  \n", counter2.read1());
+ printf("Count3 :   %d  \n", counter3.read1());
+ printf("Count4 :   %d  \n", counter4.read1());
+ printf("Count5 :   %d  \n", counter5.read1());
+ printf("Count6 :   %d  \n", counter6.read1());
+ printf("Count7 :   %d  \n", counter7.read1());
+ printf("Count8 :   %d  \n", counter8.read1());
+ printf("Count9 :   %d  \n", counter9.read1());
+ printf("Count10 :   %d  \n", counter10.read1());
+ printf("Count11 :   %d  \n", counter11.read1());
+ printf("Count12 :   %d  \n", counter12.read1());
+  printf("Count13 :   %d  \n", counter13.read1());
+
+// printf("Count A0 :   %d  \n", counterA0.read1());
+// printf("Count A1 :   %d  \n", counterA1.read1());
+// printf("Count A2 :   %d  \n", counterA2.read1());
+// printf("Count A3 :   %d  \n", counterA3.read1());
+// printf("Count A4 :   %d  \n", counterA4.read1());
+// printf("Count A5 :   %d  \n", counterA5.read1());
+// printf("Count A6 :   %d  \n", counterA6.read1());
+// printf("Count A7 :   %d  \n", counterA7.read1());
+
+
         // printf("Count5 so far 1: %d  / 2 : %d\n\n\n", counter5.read1(), counter5.read2());
         printf("\n\n\n");
         ThisThread::sleep_for(2000);
