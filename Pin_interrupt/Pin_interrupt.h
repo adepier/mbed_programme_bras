7bd2c5b39b6987c410bc9712b832e82f535574e0
adepier <adepier@gmail.com>
2022-10-27 21:24:43 +0200

    compile

---
+++ Pin_interrupt/Pin_interrupt.h
@@ -0,0 +1,22 @@
+#include "mbed.h" 
+struct  Encoder 
+{  
+ int  tic_forward =0 ;
+ int  tic_backward=0 ;   
+ int count = 0;
+};
+
+class Pin_interrupt {
+public:
+    Pin_interrupt(PinName pin , int num_compteur, Encoder &encoder)  ;
+  void  fall_1();
+  void  fall_2();
+  void  rise_2();
+  void  increment();
+
+
+
+private:
+    InterruptIn _interrupt; 
+    Encoder * _encoder ;  
+};
